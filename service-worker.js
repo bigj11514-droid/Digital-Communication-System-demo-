b09f85cc09dc7bdeddb8d0fbc67e50c83205c725
Nartey <bigj11514@gmail.com>
2026-06-25 22:42:59 +0000

    demo

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 self.addEventListener('push', (event) => {
-  const data = event.data ? event.data.json() : { title: 'New Notice', body: 'You have a new notice.' };
+  const data = event.data ? event.data.json() : { title: 'New Notice', body: 'You have a new notice from [school name here].' };
   const options = {
     body: data.body,
     icon: 'icon-192.png',
